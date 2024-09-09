2d6016f01aef956b94cd1ba538addfb241544d8e
西米西正 <ximixizheng@icloud.com>
2024-09-09 15:36:23 +0800

    11

---
+++ Europe_LemmyProject_CNs/SubModule.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using TaleWorlds.Library;
+using TaleWorlds.Localization;
+using TaleWorlds.MountAndBlade;
+
+namespace Europe_LemmyProject_CNs
+{
+    public class SubModule : MBSubModuleBase
+    {
+        protected override void OnBeforeInitialModuleScreenSetAsRoot()
+        {
+            base.OnBeforeInitialModuleScreenSetAsRoot();
+            InformationManager.DisplayMessage(new InformationMessage(((object)new TextObject("{=!}欧洲大陆汉化加载成功", (Dictionary<string, object>)null)).ToString(), Colors.Green));
+        }
+    }
+}
